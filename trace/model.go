7018f67c320f30b75b237d0b368a941d1a1f0fe0
nelsonlai <nelsonlai9503@gmail.com>
2022-09-28 11:29:49 +0800

    add trace feature

---
+++ trace/model.go
@@ -0,0 +1,51 @@
+package trace
+
+import (
+	"time"
+
+	"github.com/nelsonlai-go/sql"
+)
+
+type Model struct {
+	sql.Model
+	Version uint `gorm:"version" json:"version"`
+}
+
+type IModel interface {
+	GetID() uint
+	InitVersion()
+	IncrementVersion()
+}
+
+func (m *Model) GetID() uint {
+	return m.ID
+}
+
+func (m *Model) InitVersion() {
+	m.Version = 1
+}
+
+func (m *Model) IncrementVersion() {
+	m.Version++
+}
+
+type Trace struct {
+	Model
+	RecordID    uint      `gorm:"not null" json:"record_id"` // equal to the primary key of the record
+	TraceTime   time.Time `gorm:"not null" json:"trace_time"`
+	TraceAction string    `gorm:"not null" json:"trace_action"`
+}
+
+type ITrace interface {
+	SetRecordID(id uint)
+	SetTraceInfo(action string)
+}
+
+func (m *Trace) SetRecordID(id uint) {
+	m.RecordID = id
+}
+
+func (m *Trace) SetTraceInfo(action string) {
+	m.TraceTime = time.Now()
+	m.TraceAction = action
+}
