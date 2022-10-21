5333afba55b12599fc860f681dd17d24ebf96837
nelsonlai <nelsonlai9503@gmail.com>
2022-10-21 12:48:39 +0800

    refactor

--- test.sh
+++
@@ -1,5 +0,0 @@
-# This script performs the test using ginkgo
-mkdir -p .coverage
-go test -v $1 -cover -coverprofile=.coverage/coverage.out
-go tool cover -html=.coverage/coverage.out -o .coverage/coverage.html
-open .coverage/coverage.html
